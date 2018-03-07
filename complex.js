0b3e229b7461363a82cfcbf987d0d38f75d65e66
mhdanas97 <37138131+mhdanas97@users.noreply.github.com>
2018-03-07 14:27:39 +0200

    I am adding a new file
    it's a typescript file

---
+++ complex.js
@@ -0,0 +1,9 @@
+var Complex = /** @class */ (function () {
+    function Complex(x, y) {
+        if (x === void 0) { x = 0; }
+        if (y === void 0) { y = 0; }
+        this.x = x;
+        this.y = y;
+    }
+    return Complex;
+}());
